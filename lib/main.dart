2a6eb83ddebc28d0fc5eafb54d91b989bc8b1194
S-MARKITECH GLOBAL <s-markitechglobal@S-MARKITECHs-Mac-mini.local>
2025-10-15 14:36:31 +0530

    Firebase Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+// ignore_for_file: avoid_print
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
+import 'features/firebase/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print("✅ Firebase initialized successfully");
+  } catch (e, stackTrace) {
+    print("❌ Firebase initialization failed: $e");
+    print(stackTrace);
+  }
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
